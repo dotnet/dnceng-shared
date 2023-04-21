3440c384be81884a4880304c598e88628720f0e3
dkurepa <dkurepa@microsoft.com>
2023-04-21 12:12:45 +0200

    add editorconfig and the open source header to all files

--- src/Microsoft.DotNet.Internal.DependencyInjection/SingleClientFactory.cs
+++ src/Microsoft.DotNet.Internal.DependencyInjection/SingleClientFactory.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.DotNet.Internal.DependencyInjection;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.DotNet.Internal.DependencyInjection;
 
 public class SingleClientFactory<T> : IClientFactory<T>
 {
