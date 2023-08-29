66b773c2934bcb98ecc835c5de66e4d8c7c22796
Premek Vysoky <premek.vysoky@microsoft.com>
2023-08-29 12:48:24 +0200

    Replace `Microsoft.Azure.Services.AppAuthentication` with `Azure.Identity`

--- src/Microsoft.DncEng.CommandLineLib/Authentication/TokenCredentialProvider.cs
+++ src/Microsoft.DncEng.CommandLineLib/Authentication/TokenCredentialProvider.cs
@@ -5,12 +5,12 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
+using Microsoft.DncEng.Configuration.Extensions;
 
 namespace Microsoft.DncEng.CommandLineLib.Authentication;
 
 public class TokenCredentialProvider
 {
-    public static string MsftAdTenantId => "72f988bf-86f1-41af-91ab-2d7cd011db47";
     private readonly IConsole _console;
     private readonly InteractiveTokenCredentialProvider _interactiveTokenCredentialProvider;
 
@@ -24,8 +24,12 @@ public class TokenCredentialProvider
     {
         var creds = new List<TokenCredential>
         {
-            new AzureServiceTokenProviderCredential(MsftAdTenantId),
+            new DefaultAzureCredential(new DefaultAzureCredentialOptions()
+            {
+                TenantId = ConfigurationConstants.MsftAdTenantId,
+            })
         };
+
         if (_console.IsInteractive)
         {
             creds.Add(await _interactiveTokenCredentialProvider.GetCredentialAsync());
