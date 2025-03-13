9814009ccf573c18b9569273df505f0d30f0b30c
missymessa <mjanecke@microsoft.com>
2025-03-13 14:23:45 -0700

    Chad told me how to fix this the right way

--- test/Microsoft.DotNet.Web.Authentication.Tests/Controllers/AnonymousAttributeController.cs
+++ test/Microsoft.DotNet.Web.Authentication.Tests/Controllers/AnonymousAttributeController.cs
@@ -6,28 +6,31 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Microsoft.DotNet.Web.Authentication.Tests.Controllers;
 
-[AllowAnonymous]
 [Route("test-auth/anonymous")]
 public class AnonymousAttributeController : ControllerBase
 {
+    [AllowAnonymous]
     [Route("no")]
     public IActionResult NoAttribute()
     {
         return Ok("Anonymous:No:Value");
     }
 
+    [AllowAnonymous]
     [Route("anonymous")]
     public IActionResult AnonymousAttribute()
     {
         return Ok("Anonymous:Anonymous:Value");
     }
 
+    [Authorize, AllowAnonymous]
     [Route("any")]
     public IActionResult Any()
     {
         return Ok("Anonymous:Any:Value");
     }
 
+    [Authorize(Roles = "ActionRole"), AllowAnonymous]
     [Route("role")]
     public IActionResult RoleAttribute()
     {
