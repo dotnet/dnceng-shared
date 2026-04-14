3d20416e02438a113a2f5796ba0a1529e268af2e
Premek Vysoky <premek.vysoky@microsoft.com>
2026-04-14 12:46:02 +0200

    Change AddSingleton to TryAddSingleton for providers

--- src/Microsoft.DotNet.GitHub.Authentication/ServiceCollectionExtensions.cs
+++ src/Microsoft.DotNet.GitHub.Authentication/ServiceCollectionExtensions.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.DotNet.GitHub.Authentication;
 using Microsoft.DotNet.Services.Utility;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -13,11 +12,11 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddGitHubTokenProvider(this IServiceCollection services)
     {
-        return services
-            .TryAddSingleton<IGitHubAppTokenProvider, GitHubAppTokenProvider>()
-            .TryAddSingleton<IGitHubTokenProvider, GitHubTokenProvider>()
-            .TryAddSingleton<ExponentialRetry>()
-            .TryAddTransient<ISystemClock, SystemClock>()
-            .TryAddTransient<IInstallationLookup, InMemoryCacheInstallationLookup>();
+        services.TryAddSingleton<IGitHubAppTokenProvider, GitHubAppTokenProvider>();
+        services.TryAddSingleton<IGitHubTokenProvider, GitHubTokenProvider>();
+        services.TryAddSingleton<ExponentialRetry>();
+        services.TryAddTransient<ISystemClock, SystemClock>();
+        services.TryAddTransient<IInstallationLookup, InMemoryCacheInstallationLookup>();
+        return services;
     }
 }
