e90d025c7556b919917d0105e547721ef7275fd4
Premek Vysoky <premek.vysoky@microsoft.com>
2026-06-19 10:25:23 +0200

    Add the DevCredential

---
+++ src/Microsoft.DotNet.Internal.Credentials/DevCredential.cs
@@ -0,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Azure.Identity;
+
+namespace Microsoft.DotNet.Internal.Credentials;
+
+/// <summary>
+/// Credential suitable for local development.
+/// </summary>
+public class DevCredential : ChainedTokenCredential
+{
+    public DevCredential() : base(
+        new AzureCliCredential(),
+        new VisualStudioCredential(),
+        new VisualStudioCodeCredential(),
+        new EnvironmentCredential())
+    {
+    }
+}
